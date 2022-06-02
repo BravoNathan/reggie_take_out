0a5020754092b616e281e3546fdad75cec22173a
Zhengyang <zyli1029@gmail.com>
2022-06-02 15:04:34 +0800

    day one develop

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -84,5 +85,23 @@ public class EmployeeController {
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
 
+    }
+
+    @PostMapping
+    public R<String> save(HttpServletRequest request ,@RequestBody Employee employee){
+        log.info("新增员工，员工信息:{}", employee.toString());
+
+        //设置初始密码：并用md5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes(StandardCharsets.UTF_8)));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获取当前用户id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+        return R.success("新增员工成功");
     }
 }
